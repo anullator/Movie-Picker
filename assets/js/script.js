27a8220c2edfabb0fd89dd3ffd68e0d7261f0ae8
Cadence K <cadencehiggins@gmail.com>
2024-04-09 20:28:54 -0500

    integrated API, logs movie info into console

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,31 @@ const streamAvailBaseURL = 'https://streaming-availability.p.rapidapi.com';
 function storeTitle() {
     const title = document.getElementById('title').textContent;
     localStorage.setItem('movie', title);
-    
+
 
 }
 
 // get movies using api
+
+async function fetchdata(event) {
+    const movieInput = document.getElementById('title').value
+    const url = `https://streaming-availability.p.rapidapi.com/search/title?title=${movieInput}&country=us&show_type=all&output_language=en`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '4e07f7e371msh03925504bb9f3f0p12b002jsn5482b87d7178',
+            'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 // render movies
 

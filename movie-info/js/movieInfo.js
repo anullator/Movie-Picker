e6b824e966c28beed3c169cf9d4a71b71e6faa32
Cadence K <cadencehiggins@gmail.com>
2024-04-15 21:19:39 -0500

    fixed bug, navigates to movie info page and collects correct info

--- movie-info/js/movieInfo.js
+++ movie-info/js/movieInfo.js
@@ -1,5 +1,11 @@
 
 const movieInput = localStorage.getItem('title');
 
-fetchdata(movieInput)
-console.log(movieInput)
+async function getMovieData() {
+    const id = localStorage.getItem('selectedMovie')
+    const movie = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=6c33289f24466f62dfa52aceefb07e8a`)
+    const responseJson = await movie.json()
+    console.log(responseJson)
+}
+
+getMovieData()
